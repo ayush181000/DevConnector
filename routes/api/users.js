546d3137b6f95102735b85b3a253af8c7046c1a7
ayush181000 <ayush181000@gmail.com>
2021-09-06 16:18:15 +0530

    JWTtoken started generating

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 //Getting model here
@@ -58,7 +60,24 @@ router.post(
 
       //Return jsonwebtoken
 
-      res.send('User Registered');
+      //Creating payload
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      //Change back to 3600
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
